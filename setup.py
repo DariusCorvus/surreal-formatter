5ea230fc72b6cd92a782dc703a23731c5464c584
Darius <dariuscorvus@pm.me>
2023-03-16 14:53:37 +0100

    fix: module not found

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name="surreal-formatter",
     version="0.0.1",
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
+    package_dir={"": "src", "surreal_formatter": "src/surreal_formatter"},
+    packages=["surreal_formatter"],
+    py_modules=["main", "config", "surreal_formatter"],
     include_package_data=True,
     install_requires=["Click", "sly==0.5"],
     entry_points={"console_scripts": ["surreal-formatter = main:cli"]},
